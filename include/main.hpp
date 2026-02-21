7d4315906895a502a7d9c6987ccb7576647aa8d7
Elimond <elimondingwiza@gmail.com>
2026-02-21 13:25:44 +0100

    main.hpp changes

--- include/main.hpp
+++ include/main.hpp
@@ -1,7 +1,22 @@
 #ifndef MAIN_HPP
 # define MAIN_HPP
 
+# include <ncurses.h>
+# include <chrono>
 # include "Board.hpp"
-# include "Entity.hpp"
+
+# define BOARD_DIM 20
+# define BOARD_ROWS BOARD_DIM
+# define BOARD_COLS BOARD_DIM * 2
+
+typedef struct s_gamestate {
+	unsigned int		level;
+	unsigned long long	time;
+	unsigned int		score;
+	unsigned int		lives;
+	Player				
+	list				enemies;
+
+}				t_gamestate;
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,35 @@
 #include "../include/main.hpp"
 
-int main()
+t_gamestate	init_state()
 {
+	t_gamestate	state;
+	
+	state->level = 1;
+	state->lives = 3;
+	state->score = 0;
+	return state;
+}
 
+int main(void)
+{
 	initscr();
+	cbreak();
+	noecho();
 	refresh();
-
+	
+	t_gamestate	state = init_state();
+	while (1)
+	{
+		// input (key inputs)
+		// update state (updates buffer & internal variables/state of the game in tempo reale)
+		// render (print buffer)
+	}
 	int yMax, xMax;
 	getmaxyx(stdscr, yMax, xMax);
 
 	Board board = Board(yMax, xMax);
-
+	
 	endwin();
 
-	return 0;
+	return (0);
 }
